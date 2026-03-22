2dd6eef9a38d55eef3a8a833ce17687bc3a8b780
creynolds1986 <c.reynolds09@hotmail.co.uk>
2026-03-22 16:17:43 +0000

    Updated directories and variable for local upload directory

---
+++ Azure/Storage/AzCopy/SAS Keys/AzCopy-SAS-Upload.ps1
@@ -0,0 +1,22 @@
+# https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-v10
+
+$storageAccountName = "<storage-account-name>"
+$containerName      = "<container-name>"
+$SASToken           = Read-Host "Enter the SAS token (without the leading '?')"
+$LocalDirectory     = "d:\azcopytest"
+
+# Create a blob container
+azcopy make "https://$storageAccountName.blob.core.windows.net/$containerName`?$SASToken"
+
+# Upload a single file
+$Filename = "Test.txt"
+azcopy copy "$LocalDirectory\$Filename" "https://$storageAccountName.blob.core.windows.net/$containerName/$Filename`?$SASToken"
+
+# Upload multiple files using a wildcard
+azcopy copy "$LocalDirectory\*.*" "https://$storageAccountName.blob.core.windows.net/$containerName`?$SASToken"
+
+# Upload a directory
+azcopy copy "$LocalDirectory\new folder" "https://$storageAccountName.blob.core.windows.net/$containerName`?$SASToken" --recursive
+
+# Upload the contents of a directory (without the folder itself)
+azcopy copy "$LocalDirectory\new folder\*" "https://$storageAccountName.blob.core.windows.net/$containerName`?$SASToken" --recursive
