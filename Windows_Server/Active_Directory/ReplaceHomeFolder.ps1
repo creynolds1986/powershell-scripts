a27e8b1f3dbb862989f2d833bfc4bcb07f8348cf
creynolds1986 <c.reynolds09@hotmail.co.uk>
2026-03-23 16:54:01 +0000

    Initial Upload

---
+++ Windows_Server/Active_Directory/ReplaceHomeFolder.ps1
@@ -0,0 +1,18 @@
+﻿#Finds all users that have the old home share and replaces with the new one.
+
+$oldpath = "\\oldserver\share"
+$newpath = "\\newserver\share"
+$users = Get-ADUser -Filter * -Properties homedirectory | Where-Object { $_.homedirectory -like "$oldpath*" }
+
+foreach ($user in $users)
+{
+    $sam = $user.samaccountname
+    try
+    {
+        Set-ADUser -Identity $sam -HomeDirectory "$newpath\$sam"
+    }
+    catch
+    {
+        Write-Warning "Failed to update $sam : $_"
+    }
+}
