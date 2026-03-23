816971ceb04ff7c13e0a28467f9b8e7a2582a42c
creynolds1986 <c.reynolds09@hotmail.co.uk>
2026-03-23 16:36:49 +0000

    Initial Upload

---
+++ Windows_Server/Active_Directory/RemoveLoginScripts.ps1
@@ -0,0 +1,11 @@
+﻿#Finds all users with a login script and removes it.
+
+$users=Get-ADUser -Filter * -Properties scriptpath | Where-Object scriptpath
+
+foreach ($user in $users)
+{
+
+$sam=$user.samaccountname
+
+    Set-ADUser -Identity $sam -Clear scriptpath
+}
